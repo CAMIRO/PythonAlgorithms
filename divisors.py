5fbad533446115ad19e130a8b8f198eb38f2e3cd
CAMIRO <juancamilosc@hotmail.com>
2021-02-17 17:34:45 +0800

    divisors algorithm - delet imported library

--- divisors.py
+++ divisors.py
@@ -1,4 +1,3 @@
-from typing import List
 # Create a function named divisors/Divisors that takes an integer n > 1 and returns an array
 #  with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. 
 # If the number is prime return the string '(integer) is prime' (null in C#)
