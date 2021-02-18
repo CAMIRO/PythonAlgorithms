6d3a89df37da90a98e8569a9db3069cb728c646a
Juan Sarmiento <juancamilosc@hotmail.com>
2021-02-18 15:58:59 +0800

    Update takeAtenMinueWalk.py

--- algorithms/takeAtenMinueWalk.py
+++ algorithms/takeAtenMinueWalk.py
@@ -47,6 +47,8 @@ if __name__ == '__main__':
 # another approaches :
 
 # approach 1: 
+# NOTE: using the python method： count()
+
 #     def is_valid_walk(walk):
 #     return len(walk) == 10 and walk.count('n') == walk.count('s') and walk.count('e') == walk.count('w')
 
