376ce1fc0e8744991f34615df33bfabd96f25dd8
CAMIRO <juancamilosc@hotmail.com>
2021-02-19 17:05:27 +0800

    adding oppositeNUmber algorithm in fundamental

---
+++ fundamentals/oppositeNumber.py
@@ -0,0 +1,12 @@
+# Very simple, given a number, find its opposite.
+# Examples:
+# 1: -1
+# 14: -14
+# -34: 34
+
+def opposite(number):
+    return number - 2 * number
+
+
+if __name__ == '__main__':
+    print(opposite(16648345.818997338))
