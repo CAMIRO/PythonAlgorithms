61417a5719a6b50dc79eade50ed30c3bcc4d4a74
CAMIRO <juancamilosc@hotmail.com>
2021-02-20 11:48:58 +0800

    adding ordenamiento por mezcla

--- algorithms/busquedaMerge.py
+++ algorithms/busquedaMerge.py
@@ -7,26 +7,42 @@ def merge_sort(lista):
         medio = len(lista) // 2
         izquierda = lista[:medio]
         derecha = lista[medio:]
+        print(izquierda, '*' * 5, derecha)
 
-    # Llamada recursiva en cada mitad 
-    merge_sort(izquierda)
-    merge_sort(derecha)
+        # Llamada recursiva en cada mitad 
+        merge_sort(izquierda)
+        merge_sort(derecha)
     
-    # Iteradores para recorer las 2 sublistas
-    i = 0
-    j = 0 
-    # Iterador para la lista principal
-    k = 0
+        # Iteradores para recorer las 2 sublistas
+        i = 0
+        j = 0 
+        # Iterador para la lista principal
+        k = 0
 
-    while i < len(izquierda) and j < len(derecha):
-        if izquierda[i] < derecha[j]:
+        while i < len(izquierda) and j < len(derecha):
+            if izquierda[i] < derecha[j]:
+                lista[k] = izquierda[i]
+                i += 1
+            else: 
+                lista[k] = derecha[j]
+                j += 1
+            k += 1 
+
+        while i < len(izquierda):
             lista[k] = izquierda[i]
             i += 1
-        else: 
+            k += 1
+
+        while j < len(derecha):
             lista[k] = derecha[j]
             j += 1
-        k += 1 
-    
+            k += 1 
+
+        print(f'izquierda {izquierda}, derecha {derecha}')
+        print(lista)
+        print('-' * 50)
+        
+    return lista
 
 
 if __name__ == '__main__':
