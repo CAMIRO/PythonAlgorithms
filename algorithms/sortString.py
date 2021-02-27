2e8ff72d2b50bd217d0e7d5539d5878c28fb6750
CAMIRO <juancamilosc@hotmail.com>
2021-02-27 17:39:23 +0800

    adding sortString

---
+++ algorithms/sortString.py
@@ -0,0 +1,15 @@
+import re
+
+def order(sentence):
+
+    X = sentence.split() #1. make a list from a string
+    Y = re.findall("\d+", sentence) #2. Getting the number from string's words 
+    Z = [x for _,x in sorted(zip(Y,X))] #3 using a list comprehension extract the first elements of each pair from the sorted, zipped list.
+    print(X)
+    print(Y)
+    print(Z)
+    return ' '.join(Z)
+    
+
+if __name__ == '__main__':
+    print(order('is2 Thi1s T4est 3a'))
